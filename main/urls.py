c5c31bba2bdef19dfe41a5f0f69d274e5dd28954
Felipe Jorquera <felipe.jorquera1997@gmail.com>
2018-11-26 17:06:15 -0300

    agrego html

--- main/urls.py
+++ main/urls.py
@@ -35,5 +35,6 @@ urlpatterns = [
     #registro
     path('login', auth_views.LoginView.as_view(), name="login"),
     path('logout', auth_views.LogoutView.as_view(), name="logout"),
+    #evento
     
 ]
